47695c22cbcc59469b4e60f601ab0dfa1fbe6fad
kwonsongmi <ksongmi0727@gmail.com>
2023-03-21 23:45:18 +0900

    Chapter1

--- src/main/kotlin/Chapter1.kt
+++ src/main/kotlin/Chapter1.kt
@@ -1,48 +1,39 @@
-// 1_1 : 일반적인 Hello World 프로그램
-fun main1(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
+import java.io.*
 
-    // Kotlin return 값이 없는 이유 : 리턴이 없는 함수를 만들었기 때문에
-    // 'fun main() : Int { '에서 Int 값이 리턴
+// 백준 알고리즘
+
+// 1000번. A+B
+// 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+fun main1000() = with(Scanner(System.`in`)) {
+    println(nextInt() + nextInt())
 }
 
-//1_2 : println() 함수가 여러 개인 프로그램
-fun main2() {
-    println("Hello World!")
-    println("Merry Christmas")
-    println("Happy New Year")
+// 1000번 BufferedReader 방법
+//    import java.io.*
+//
+//    val br = BufferedReader(InputStreamReader(System.`in`))
+//
+//    fun main() {
+//        val bw = BufferedWriter(OutputStreamWriter(System.out))
+//        val (a,b) = br.readLine()!!.split(" ").map{it.toInt()}
+//        val plus = a+b
+//        bw.write("$plus")
+//        bw.close()
+//
+//    }
 
-    println("Hello World!")
-    println("Merry Christmas")
-    println("Happy New Year")
 
-    println("Hello World!")
-    println("Merry Christmas")
-    println("Happy New Year")
+// 1008번. A / B
+fun main1008()  = with(Scanner(System.`in`)) {
+    println(nextDouble() / nextDouble())
 }
 
-// 1_3 : 1_2를 for문을 사용한 프로그램
-fun main3() {
-    for (i in 0..2) {
-        println("Hello World!")
-        println("Merry Christmas")
-        println("Happy New Year")
-    }
-}
-
-// 1_4 : 무식한 검색 프로그램
-fun main() {
-    val input = 0
-
-    // 배열의 크기만 지정하고 값은 나중에 저장할 경우 arrayOfNulls(size) 함수를 이용, 배열 선언과 동시에 값을 초기화 할 경우 arrayOf(value) 함수를 이용
-    val data = arrayOfNulls<Int>(1000)
-
-    for(i in 0..999) {
-        data[i] = i+1
-    }
-
-    println("찾을 값을 입력하세요 => ")
-
-
-
+fun mainB_1008() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (a,b) = br.readLine()!!.split(" ").map{it.toDouble()}
+    val div = a / b
+    bw.write("$div")
+    bw.close()
 }
