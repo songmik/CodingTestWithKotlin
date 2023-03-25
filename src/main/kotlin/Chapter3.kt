e47f40ae22b1cc809e130836dd552e99e2e9fe0b
kwonsongmi <ksongmi0727@gmail.com>
2023-03-25 12:17:58 +0900

    Chapter3_2

--- src/main/kotlin/Chapter3.kt
+++ src/main/kotlin/Chapter3.kt
@@ -12,7 +12,7 @@ fun main_2739() {
 }
 
 // 10950. A+B-3
-fun main() {
+fun main_10950() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val a = br.readLine()!!.toInt()
 
@@ -23,10 +23,48 @@ fun main() {
 }
 
 // 8393. 합
+fun main_8393() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine()!!.toInt()
+    var result = 0
+
+    for(i in 1..n) {
+       result += i
+    }
+    println(result)
+}
 
 // 25304. 영수증
+fun main_25304() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val X = br.readLine()!!.toInt()
+    val N = br.readLine()!!.toInt()
+    var total = 0
+
+    for(i in 1..N) {
+        val (a, b) = br.readLine().split(" ").map { it.toInt() }
+        total += a*b
+    }
+
+    if (X == total) {
+        println("Yes")
+    } else {
+        println("No")
+    }
+}
 
 // 25314. 코딩은 체육과목 입니다
+fun main_25314() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val sb = StringBuilder()
+    val N = br.readLine()!!.toInt()
+
+    for (i in 0 until N/4) {
+        sb.append("long ")
+    }
+    sb.append("int")
+    println(sb)
+}
 
 // 15552. 빠른 A+B
 
